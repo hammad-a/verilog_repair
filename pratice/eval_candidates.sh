01481590436eb112bcbdf8c7e1125a7668c9d5d1
Hammad Ahmad <hammada@umich.edu>
2020-05-15 19:42:13 -0400

    a working version of eval_candidates

--- pratice/eval_candidates.sh
+++ pratice/eval_candidates.sh
@@ -3,17 +3,19 @@
 # how to run this bash script: ./eval_candidates.sh <candidates_dir>
 module load vcs/2017.12-SP2-1
 
-candidatesDir=$2
+candidatesDir=$1
 
 DONE=0
 
 date>start_time.txt
 
-for filename in $candidatesDir; do 
+echo $candidatesDir
 
-    cat "$filename"
+for filename in $candidatesDir/*.v; do 
 
-    #timeout 20 vcs -sverilog +vc -Mupdate -line -full64 sys_defs.vh first_counter_tb_t2.v ./candidates/candidate_$i\_$j.v  -o simv -R 
+    #cat "$filename"
+
+    timeout 20 vcs -sverilog +vc -Mupdate -line -full64 first_counter/sys_defs.vh first_counter/first_counter_tb_t1.v "$filename"  -o simv -R 
 
 done
 
