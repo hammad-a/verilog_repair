341a0c7943f8a8875f74243f23cb955a09b52906
Hammad Ahmad <hammada@umich.edu>
2020-05-17 17:02:08 -0400

    bug fixes, cmp not being used correctly

--- practice/eval_candidates.sh
+++ practice/eval_candidates.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# how to run this bash script: ./eval_candidates.sh <candidates_dir>
+# how to run this bash script: ./eval_candidates.sh <candidates_dir> <testbench.v>
 module load vcs/2017.12-SP2-1
 
 candidatesDir=$1
+tb=$2
 
 DONE=0
 
@@ -11,11 +12,27 @@ date>start_time.txt
 
 echo $candidatesDir
 
+timeout 20 vcs -sverilog +vc -Mupdate -line -full64 first_counter/sys_defs.vh $tb first_counter/first_counter.v  -o simv -R | tee output_oracle.txt
+python strip_vcs_output.py output_oracle.txt
+
+rm repairs.txt
+touch repairs.txt
+
 for filename in $candidatesDir/*.v; do 
 
     #cat "$filename"
+    rm output.txt
+    rm output_stripped.txt
 
-    timeout 20 vcs -sverilog +vc -Mupdate -line -full64 first_counter/sys_defs.vh first_counter/first_counter_tb_t1.v "$filename"  -o simv -R 
+    timeout 20 vcs -sverilog +vc -Mupdate -line -full64 first_counter/sys_defs.vh $tb "$filename"  -o simv -R | tee output.txt 
+    python strip_vcs_output.py output.txt
+    
+    cmp output_stripped.txt output_oracle_stripped.txt
+
+    if [ $? -eq 0  ]; then
+	echo "REPAIR FOUND!!!!!!!"
+	echo $filename >> repairs.txt
+    fi  
 
 done
 
