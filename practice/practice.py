63799b388f10dee80268c00ad19d9ad5abad31c7
Hammad Ahmad <hammada@umich.edu>
2020-05-19 15:44:10 -0400

    implemeted crapvisitor

--- practice/practice.py
+++ practice/practice.py
@@ -1,13 +1,14 @@
 import sys
 import os
 from optparse import OptionParser
+
 # genprog: Class Rep: you need to write your own class 
 # the next line can be removed after installation
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from pyverilog.vparser.parser import parse
 from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
-
+import pyverilog.vparser.ast as vast
 
 class CandidateCollector(ASTCodeGenerator):
     def __init__(self):
@@ -33,14 +34,24 @@ class FixCollector(ASTCodeGenerator):
             self.visit(c)
 
 
-#custom visitor class that updates expressions of the form:
+# custom visitor class that updates expressions of the form:
 #   symbolName <= x
 # to become
 #   symbolName <= x+1
 class CrapVisitor(ASTCodeGenerator):
+
+    def addToBVal(self, bVal, num):
+        tmp = int(bVal.split('b')[1], 2)
+        numBits = int(bVal.split('\'')[0])
+        tmp += num
+        tmpstr = ('{0:0'+str(numBits)+'b}').format(tmp)
+        if len(tmpstr) > numBits: # drop the most significant bit if the length after incrementing is longer than the number of bits
+            tmpstr = tmpstr[len(tmpstr)-numBits:]
+        return str(numBits) + "'b" + tmpstr
+
     def visit(self, ast):
-        #ast is node 
-        #print(ast.__class__.__name__)
+        # ast is node 
+        # print(ast.__class__.__name__)
 
         #  Expressions with "<=" are 'nonblockingsubstitutions' in the
         #  abstract syntax
@@ -55,19 +66,15 @@ class CrapVisitor(ASTCodeGenerator):
             # now, we only want expressions that have an identifier on
             # the left (i.e., a net name) and a int constant on the
             # right (some [0-9]+)
-            if my_lvalue.__class__.__name__ == 'Identifier' \
-                    and my_rvalue.__class__.__name__ == 'IntConst':
-                #print("Updating {} from {} to {}".format(my_lvalue.name,
-                #    my_rvalue.value,
-                #    int(my_rvalue.value)+1))
+            if my_lvalue.__class__.__name__ == 'Identifier' and my_rvalue.__class__.__name__ == 'IntConst':
                 # apparently, this ast structure stores everything as
-                # strings, we to increment, we coerce to an int, add 1,
+                # strings, we need to increment, we coerce to an int, add 1,
                 # then coerce back to a string
-                #my_rvalue.value = str( int(my_rvalue.value) + 1 )
-                pass
-
-
-            
+                incrementedVal = self.addToBVal(my_rvalue.value, 1)
+                print("Updating {} from {} to {}".format(my_lvalue.name,
+                    my_rvalue.value,
+                   incrementedVal))
+                my_rvalue.value = incrementedVal
 
         #if ast.__class__.__name__ == "IntConst":
         #    print("Found int const {}".format(ast.value))
@@ -119,6 +126,7 @@ def main():
     print ("!!")
 
     ast.show()
+    # print("\n\n\n")
 
     candidatecollector = CandidateCollector()
     candidatecollector.visit(ast)
@@ -128,39 +136,43 @@ def main():
     fixcollector.visit(ast)
     #print(fixcollector.my_fixes)
 
-#    crapvisitor = CrapVisitor()
-#    print(crapvisitor.visit(ast))
+    crapvisitor = CrapVisitor()
+    print(crapvisitor.visit(ast))
+
+    ast.show()
 
     codegen = ASTCodeGenerator()
     rep_num=0
 
-    try:
-        dirName = os.getcwd()+"/pv_candidates"
-        os.mkdir(dirName)
-        print("Directory " , dirName ,  " created ") 
-    except:
-        print("Directory " , dirName ,  " already exists")
+    print(codegen.visit(ast))
 
-    for f in fixcollector.my_fixes:
-        for c in candidatecollector.my_candidates:
-            old_id = f.name
-            f.name = c
+    # try:
+    #     dirName = os.getcwd()+"/pv_candidates"
+    #     os.mkdir(dirName)
+    #     print("Directory " , dirName ,  " created ") 
+    # except:
+    #     print("Directory " , dirName ,  " already exists")
+
+    # for f in fixcollector.my_fixes:
+    #     for c in candidatecollector.my_candidates:
+    #         old_id = f.name
+    #         f.name = c
 
 
-            print('Repair # {}'.format(rep_num))
-            print('Changing {} to {}'.format(old_id, c))
+    #         print('Repair # {}'.format(rep_num))
+    #         print('Changing {} to {}'.format(old_id, c))
 
-            rslt = codegen.visit(ast)
-            print(rslt)
+    #         rslt = codegen.visit(ast)
+    #         print(rslt)
 
-            outf = open("./pv_candidates/candidate_"+str(rep_num)+".v","w+")
-            outf.write(str(rslt))
-            outf.close()
+    #         outf = open("./pv_candidates/candidate_"+str(rep_num)+".v","w+")
+    #         outf.write(str(rslt))
+    #         outf.close()
 
-            print('$$$$$$$$$')
+    #         print('$$$$$$$$$')
 
-            f.name = old_id
-            rep_num = rep_num+1
+    #         f.name = old_id
+    #         rep_num = rep_num+1
 
 if __name__ == '__main__':
     main()
