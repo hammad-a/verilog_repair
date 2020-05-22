a61c9e975867a5b15406eaa60c1bd33e117f5ce0
Hammad Ahmad <hammada@umich.edu>
2020-05-21 21:50:26 -0400

    added functions for random mutations and all mutations

--- prototype.py
+++ prototype.py
@@ -2,6 +2,7 @@ import sys
 import os
 from optparse import OptionParser
 import copy
+import random
 
 # genprog: Class Rep: you need to write your own class 
 # the next line can be removed after installation
@@ -15,7 +16,7 @@ import pyverilog.vparser.ast as vast
 """
 Valid mutation operators supported by the algorithm.
 """
-VALID_MUTATIONS = ["swap_plus_minus", "increment_identifier"]
+VALID_MUTATIONS = ["swap_plus_minus", "increment_identifier", "decrement_identifier", "flip_if_cond"]
 
 """
 Returns a set of line numbers as potential targets for mutations.
@@ -25,7 +26,7 @@ class CandidateCollector(ASTCodeGenerator):
         self.my_candidates = set()
 
     def visit(self, ast):
-        if ast.__class__.__name__ in [ 'BlockingSubstitution', 'NonblockingSubstitution' ]:
+        if ast.__class__.__name__ in [ 'BlockingSubstitution', 'NonblockingSubstitution', 'IfStatement' ]:
             self.my_candidates.add(ast.lineno)
 
         for c in ast.children():
@@ -45,15 +46,6 @@ class Mutate(ASTCodeGenerator):
         self.mutation = "null"
         self.mutateAt = -1
 
-    def addToBVal(self, bVal, num):
-        tmp = int(bVal.split('b')[1], 2)
-        numBits = int(bVal.split('\'')[0])
-        tmp += num
-        tmpstr = ('{0:0'+str(numBits)+'b}').format(tmp)
-        if len(tmpstr) > numBits: # drop the most significant bit if the length after incrementing is longer than the number of bits
-            tmpstr = tmpstr[len(tmpstr)-numBits:]
-        return str(numBits) + "'b" + tmpstr
-
     def visit(self, ast):
 
         if self.mutation == "swap_plus_minus":
@@ -63,7 +55,7 @@ class Mutate(ASTCodeGenerator):
                     print("Changing %s on line %s to %s" % (ast.right.var, ast.right.var.lineno, new_child))
                     ast.right.var = new_child
                 elif ast.right.var.__class__.__name__ == "Minus" and ast.right.var.lineno == self.mutateAt:
-                    new_child = vast.Plus(ast.right.var.left, ast.right.var.right)
+                    new_child = vast.Plus(ast.right.var.left, ast.right.var.right, ast.right.var.lineno)
                     print("Changing %s on line %s to %s" % (ast.right.var, ast.right.var.lineno, new_child))
                     ast.right.var = new_child
         elif self.mutation == "increment_identifier":
@@ -71,11 +63,24 @@ class Mutate(ASTCodeGenerator):
                 my_lvalue = ast.left.var
                 my_rvalue = ast.right.var
                 if my_lvalue.__class__.__name__ == 'Identifier' and my_rvalue.__class__.__name__ == 'IntConst' and my_rvalue.lineno == self.mutateAt:
-                    incrementedVal = self.addToBVal(my_rvalue.value, 1)
-                    print("Updating {} from {} to {}".format(my_lvalue.name,
-                        my_rvalue.value,
-                    incrementedVal))
+                    incrementedVal = my_rvalue.value + " + 1"
+                    print("Updating %s on line %d from %s to %s" % (my_lvalue.name, my_rvalue.lineno, my_rvalue.value, incrementedVal))
                     my_rvalue.value = incrementedVal
+        elif self.mutation == "decrement_identifier":
+            if ast.__class__.__name__ == 'NonblockingSubstitution':
+                my_lvalue = ast.left.var
+                my_rvalue = ast.right.var
+                if my_lvalue.__class__.__name__ == 'Identifier' and my_rvalue.__class__.__name__ == 'IntConst' and my_rvalue.lineno == self.mutateAt:
+                    decrementedVal = my_rvalue.value + " - 1"
+                    print("Updating %s on line %d from %s to %s" % (my_lvalue.name, my_rvalue.lineno, my_rvalue.value, decrementedVal))
+                    my_rvalue.value = decrementedVal
+        elif self.mutation == "flip_if_cond":
+            if ast.__class__.__name__ == 'IfStatement' and ast.lineno == self.mutateAt:
+                if ast.cond.__class__.__name__ == "Eq":
+                    new_cond = vast.NotEq(ast.cond.left, ast.cond.right, ast.lineno)
+                    print("Changing %s on line %s to %s" % (ast.cond, ast.lineno, new_cond))
+                    ast.cond = new_cond
+
         elif self.mutation not in VALID_MUTATIONS or self.mutateAt == -1:
             print("Not a valid mutation: %s at line %d" % (self.mutation, self.mutateAt))
 
@@ -128,20 +133,47 @@ def main():
 
     mutation_op = Mutate()
 
-    attempts = 0
-    valids = 0
+    # twoDistanceEdits = True
+    # try_all_mutations(mutation_op, list(candidatecollector.my_candidates), codegen, ast, twoDistanceEdits)
+
+    try_random_mutations(mutation_op, list(candidatecollector.my_candidates), codegen, ast, 500)
+
+def try_all_mutations(mutation_op, candidates, codegen, ast, furtherEdits, attempts=0, valids=0):
     for choice in VALID_MUTATIONS:
-        for line in candidatecollector.my_candidates:
+        for line in candidates:
             mutation_op.set_mutation(choice, line)
             tmp = copy.deepcopy(ast)
             mutation_op.visit(tmp)
+            mutation_op.reset_mutation()
             if tmp != ast: # if the mutation was successful
                 print(codegen.visit(tmp))
                 print("#################\n")
+                if furtherEdits:
+                    try_all_mutations(mutation_op, candidates, codegen, tmp, False, attempts, valids)
                 valids += 1
             attempts += 1
+    if not furtherEdits:
+        print("A total of %d mutations were successful out of %d attempted mutations." % (valids, attempts))
+
+def try_random_mutations(mutation_op, candidates, codegen, ast, maxIters):
+    uniq = set()
+    for i in range(maxIters):
+        # choose a random mutation and line; might not be a valid mutation
+        choice = random.choice(VALID_MUTATIONS)
+        line = random.choice(candidates)
+        mutation_op.set_mutation(choice, line)
+        tmp = copy.deepcopy(ast)
+        mutation_op.visit(tmp)
+        mutation_op.reset_mutation()
+        if tmp != ast: # if the mutation was successful and/or resulted in a different ast
+            uniq.add(tmp)
     
-    print(valids,attempts)
+    for tmp in uniq:
+        print(codegen.visit(tmp))
+        print("#################\n")
+
+    print("A total of %d mutations were successful out of %d attempted mutations." % (len(uniq), maxIters))
+
 
 
 if __name__ == '__main__':
