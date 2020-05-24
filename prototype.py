03de904c5f0a2324bae72749c2d768e65c1e53e4
Hammad Ahmad <hammada@umich.edu>
2020-05-24 12:37:10 -0400

    added mutation to swap identifier names

--- prototype.py
+++ prototype.py
@@ -17,12 +17,12 @@ import pyverilog.vparser.ast as vast
 Valid mutation operators supported by the algorithm.
 """
 VALID_MUTATIONS = ["swap_plus_minus", "increment_identifier", "decrement_identifier", "increment_rhand_eq", "decrement_rhand_eq",
-    "flip_if_cond", "flip_all_sens_edge", "flip_random_sens_edge", "increment_cond_vals", "decrement_cond_vals"]
+    "flip_if_cond", "flip_all_sens_edge", "flip_random_sens_edge", "increment_cond_vals", "decrement_cond_vals", "change_identifier_name"]
 
 """
 Valid mutation operators supported by the algorithm.
 """
-MUTATIONS_TARGETS = ["BlockingSubstitution", "NonblockingSubstitution", "IfStatement", "SensList", "Eq", "Cond"]
+MUTATIONS_TARGETS = ["BlockingSubstitution", "NonblockingSubstitution", "IfStatement", "SensList", "Eq", "Cond", "Identifier"]
 
 WRITE_TO_FILE = True
 
@@ -32,19 +32,23 @@ Returns a set of line numbers as potential targets for mutations.
 class CandidateCollector(ASTCodeGenerator):
     def __init__(self):
         self.my_candidates = set()
+        self.my_identifiers = set()
 
     def visit(self, ast):
         if ast.__class__.__name__ in MUTATIONS_TARGETS:
             self.my_candidates.add(ast.lineno)
+        if ast.__class__.__name__ == "Identifier":
+            self.my_identifiers.add(ast.name)
 
         for c in ast.children():
             self.visit(c)
 
 class Mutate(ASTCodeGenerator):
 
-    def __init__(self):
+    def __init__(self, identifiers):
         self.mutation = "null"
         self.mutateAt = -1
+        self.identifiers = identifiers
 
     def set_mutation(self, mutation, lineno):
         self.mutation = mutation
@@ -66,6 +70,7 @@ class Mutate(ASTCodeGenerator):
                     new_child = vast.Plus(ast.right.var.left, ast.right.var.right, ast.right.var.lineno)
                     print("Changing %s on line %s to %s" % (ast.right.var, ast.right.var.lineno, new_child))
                     ast.right.var = new_child
+
         elif self.mutation == "increment_identifier":
             if ast.__class__.__name__ == 'NonblockingSubstitution':
                 my_lvalue = ast.left.var
@@ -78,6 +83,7 @@ class Mutate(ASTCodeGenerator):
                     new_child = vast.Plus(my_rvalue, vast.IntConst(1))
                     print("Changing %s on line %s to %s" % (my_rvalue, ast.right.var.lineno, new_child))
                     ast.right.var = new_child
+
         elif self.mutation == "decrement_identifier":
             if ast.__class__.__name__ == 'NonblockingSubstitution':
                 my_lvalue = ast.left.var
@@ -90,36 +96,42 @@ class Mutate(ASTCodeGenerator):
                     new_child = vast.Minus(my_rvalue, vast.IntConst(1))
                     print("Changing %s on line %s to %s" % (my_rvalue, ast.right.var.lineno, new_child))
                     ast.right.var = new_child
+
         elif self.mutation == "increment_rhand_eq":
             if ast.__class__.__name__ == 'Eq':
                 if ast.right.__class__.__name__ == 'IntConst' and ast.right.lineno == self.mutateAt:
                     incrementedVal = ast.right.value + " + 1"
                     print("Changing %s on line %s to %s" % (ast.right, ast.right.lineno, incrementedVal))
                     ast.right.value = incrementedVal
+
         elif self.mutation == "decrement_rhand_eq":
             if ast.__class__.__name__ == 'Eq':
                 if ast.right.__class__.__name__ == 'IntConst' and ast.right.lineno == self.mutateAt:
                     decrementedVal = ast.right.value + " - 1"
                     print("Changing %s on line %s to %s" % (ast.right, ast.right.lineno, decrementedVal))
                     ast.right.value = decrementedVal
+
         elif self.mutation == "flip_if_cond":
             if ast.__class__.__name__ == 'IfStatement' and ast.lineno == self.mutateAt:
                 if ast.cond.__class__.__name__ == "Eq":
                     new_cond = vast.NotEq(ast.cond.left, ast.cond.right, ast.lineno)
                     print("Changing %s on line %s to %s" % (ast.cond, ast.lineno, new_cond))
                     ast.cond = new_cond
+
         elif self.mutation == "flip_all_sens_edge":
             if ast.__class__.__name__ == 'SensList' and ast.lineno == self.mutateAt:
                 for sens in ast.list:
                     newType = random.choice(("posedge", "negedge", "level", "all"))
                     print("Changing sens %s type on line %s from %s to %s" % (sens.sig, ast.lineno, sens.type, newType))
                     sens.type = newType
+
         elif self.mutation == "flip_random_sens_edge":
             if ast.__class__.__name__ == 'SensList' and ast.lineno == self.mutateAt:
                 sens = random.choice(ast.list)
                 newType = random.choice(("posedge", "negedge", "level", "all"))
                 print("Changing sens %s type on line %s from %s to %s" % (sens.sig, ast.lineno, sens.type, newType))
                 sens.type = newType
+
         elif self.mutation == "increment_cond_vals":
             if ast.__class__.__name__ == "Cond" and ast.lineno == self.mutateAt:
                 p = random.random()
@@ -131,6 +143,7 @@ class Mutate(ASTCodeGenerator):
                     incrementedVal = ast.false_value.value + " + 1"
                     print("Changing the false value %s on line %s to %s" % (ast.false_value, ast.false_value.lineno, incrementedVal))
                     ast.false_value.value = incrementedVal
+
         elif self.mutation == "decrement_cond_vals":
             if ast.__class__.__name__ == "Cond" and ast.lineno == self.mutateAt:
                 p = random.random()
@@ -142,6 +155,13 @@ class Mutate(ASTCodeGenerator):
                     decrementedVal = ast.false_value.value + " - 1"
                     print("Changing the false value %s on line %s to %s" % (ast.false_value, ast.false_value.lineno, decrementedVal))
                     ast.false_value.value = decrementedVal
+        
+        elif self.mutation == "change_identifier_name":
+            if ast.__class__.__name__ == "Identifier" and ast.lineno == self.mutateAt:
+                newName = random.choice(self.identifiers)
+                print("Changing the identifier name at line %s from %s to %s" % (ast.lineno, ast.name, newName))
+                ast.name = newName
+
         elif self.mutation not in VALID_MUTATIONS or self.mutateAt == -1:
             print("Not a valid mutation: %s at line %d" % (self.mutation, self.mutateAt))
 
@@ -192,7 +212,7 @@ def main():
     candidatecollector = CandidateCollector()
     candidatecollector.visit(ast)
 
-    mutation_op = Mutate()
+    mutation_op = Mutate(list(candidatecollector.my_identifiers))
 
     try:
         dirName = os.getcwd()+"/repair_candidates"
