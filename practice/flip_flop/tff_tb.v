396ac5419d8766697d175592cac36ecd442995ac
Hammad Ahmad <hammada@umich.edu>
2020-05-30 19:55:23 -0400

    minor

--- practice/flip_flop/tff_tb.v
+++ practice/flip_flop/tff_tb.v
@@ -9,11 +9,16 @@ module tb;
           .q(q));
  
   always #5 clk = ~clk;
+  
+  integer f;
+  initial begin
+    f = $fopen("output.txt");
+    $fmonitor (f, "T=%0t rstn=%0b t=%0d q=%0d", $time, rstn, t, q);
+  end
  
   initial begin  
     {rstn, clk, t} <= 0;
  
-    $monitor ("T=%0t rstn=%0b t=%0d q=%0d", $time, rstn, t, q);
     repeat(2) @(posedge clk);
     rstn <= 1;
  
@@ -22,6 +27,7 @@ module tb;
       #(dly) t <= $random;
     end
   #20 $finish;
+  $fclose(f);
   end
 endmodule
 
