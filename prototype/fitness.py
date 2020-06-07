69be7591acceb4cb5cb22ab78935d845c8acf832
Hammad Ahmad <hammada@umich.edu>
2020-06-07 19:08:29 -0400

    fitness with bit weights

--- prototype/fitness.py
+++ prototype/fitness.py
@@ -10,7 +10,32 @@ def strip_bits(bits):
         bits[i] = bits[i].strip()
     return bits
 
-def calculate_fitness(oracle, sim):
+# parse the weights file
+def get_weights(weights_file, oracle):
+    weights_by_varname = dict()
+    weights_by_bit = dict()
+    
+    for line in weights_file:
+        line = line.strip()
+        tmp = line.split("=")
+        weights_by_varname[tmp[0]] = float(tmp[1])
+    
+    bits = strip_bits(oracle[0].split(","))
+    for i in range(1, len(bits)):
+        varname = bits[i]
+        if "[" in varname:
+            varname = varname.split("[")[0]
+        weights_by_bit[i] = weights_by_varname.get(varname)
+    
+    return weights_by_bit
+
+def calculate_fitness(oracle, sim, weights_file):
+
+    weights = None
+    if weights_file:
+        weights = get_weights(weights_file, oracle)
+    print(weights)
+    
     if len(oracle) != len(sim): # TODO: change this to append the sim file to match oracle length with x-bits
         # resize_sim(oracle, sim)
         return 0
@@ -25,31 +50,34 @@ def calculate_fitness(oracle, sim):
         # cycle_weight = 1/(2**(i-1))
         cycle_weight = 1
         
-        # print("Clock cycle: %s" % clk)
-        # print("Clocl cycle number: %s" % i)
         for b in range(len(tmp_oracle)):
+            if weights:
+                bit_weight = weights[b+1] # off-set by 1 since time not included in b
+            else:
+                bit_weight = 1
+
             if (tmp_oracle[b], tmp_sim[b]) in (('0', '0'), ('1', '1')):
-                fitness += cycle_weight * 1
-                total_possible += cycle_weight * 1
+                fitness += bit_weight * cycle_weight * 1
+                total_possible += bit_weight * cycle_weight * 1
             elif (tmp_oracle[b], tmp_sim[b]) == ('x', 'x'):
-                fitness += cycle_weight * X_WEIGHT * 1
-                total_possible += cycle_weight * X_WEIGHT * 1
+                fitness += bit_weight * cycle_weight * X_WEIGHT * 1
+                total_possible += bit_weight * cycle_weight * X_WEIGHT * 1
             elif (tmp_oracle[b], tmp_sim[b]) in (('0', '1'), ('1', '0')):
                 if DEBUG: print("Mismatch in bit %s of clock cycle %s (at time %s)..." % (b, i, clk))
-                fitness -= cycle_weight * 1
-                total_possible += cycle_weight * 1
+                fitness -= bit_weight * cycle_weight * 1
+                total_possible += bit_weight * cycle_weight * 1
             elif (tmp_oracle[b], tmp_sim[b]) in (('0', 'x'), ('1', 'x'), ('x', '0'), ('x', '1')):
                 if DEBUG: print("Mismatch in x-bit %s of clock cycle %s (at time %s)..." % (b, i, clk))
-                fitness -= cycle_weight * X_WEIGHT * 1
-                total_possible += cycle_weight * X_WEIGHT * 1
-        # print("Fitness = %s out of a total of %s" % (fitness, total_possible))
+                fitness -= bit_weight * cycle_weight * X_WEIGHT * 1
+                total_possible += bit_weight * cycle_weight * X_WEIGHT * 1
+
     return fitness, total_possible
 
 def main():
-    if len(sys.argv) != 3:
-        print("USAGE: python3 fitness.py <oracle.txt> <output.txt>")
+    if len(sys.argv) != 3 and len(sys.argv) != 4:
+        print("USAGE: python3 fitness.py <oracle.txt> <output.txt> [weights.txt]")
         sys.exit(1)
-    
+
     f = open(sys.argv[1], "r")
     oracle_lines = f.readlines()
     f.close()
@@ -58,11 +86,16 @@ def main():
     sim_lines = f.readlines()
     f.close()
 
-    fitness, total_possible = calculate_fitness(oracle_lines, sim_lines)
+    weights = None
+    if len(sys.argv) == 4:
+        f = open(sys.argv[3], "r")
+        weights = f.readlines()
+        f.close()
+        
+    fitness, total_possible = calculate_fitness(oracle_lines, sim_lines, weights)
     ff = fitness/total_possible
     if ff < 0: ff = 0
     print(ff)
-    
 
 if __name__ == '__main__':
     main()
