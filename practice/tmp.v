7180854730c71e6cf67431fc58a6a37ff518da67
Hammad Ahmad <hammada@umich.edu>
2021-01-25 11:58:09 -0500

    added cirfix produced repair changes

--- practice/tmp.v
+++ practice/tmp.v
@@ -5,8 +5,8 @@ always @ (posedge clk)
     command <= CMD_NOP;
     state_cnt <= 4'hf;
     haddr_r <= {HADDR_WIDTH{1'b0}};
-    
-    rd_data_r <= data;
+    state_cnt_next <= 4'd0;
+    rd_data_r <= IDLE;
     busy <= 1'b0;
     end
   else
