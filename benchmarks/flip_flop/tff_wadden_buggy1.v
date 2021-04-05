e2c5de365cf789a04752319f24997d0ca55dcfd7
Hammad Ahmad <hammada@umich.edu>
2021-04-05 14:34:37 -0400

    added flipflop benchmarks

---
+++ benchmarks/flip_flop/tff_wadden_buggy1.v
@@ -0,0 +1,16 @@
+module tff (   input clk,
+               input 	  rstn,
+               input 	  t,
+               output reg q);
+   
+   always @ (posedge clk) begin
+      
+      if (rstn)
+	 q <= 0;
+      else if (t)
+	 q <= ~q;
+      else
+	 q <= q;
+      
+   end
+endmodule
