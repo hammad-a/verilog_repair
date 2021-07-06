ca8f4b155896d55b884278b3c99edced8d78fa79
Hammad Ahmad <hammada@umich.edu>
2021-07-06 11:50:13 -0400

    updated tff tb

--- benchmarks/flip_flop/tff_tb.v
+++ benchmarks/flip_flop/tff_tb.v
@@ -2,29 +2,37 @@ module tb;
   reg clk;
   reg rstn;
   reg t;
- 
+
   tff u0 (  .clk(clk),
             .rstn(rstn),
             .t(t),
           .q(q));
- 
-  always #5 clk = ~clk;
- 
-  integer f;
-  initial begin  
-    {rstn, clk, t} <= 0;
-    f = $fopen("output.txt");
 
-    $fmonitor(f, "T=%0t rstn=%0b t=%0d q=%0d", $time, rstn, t, q);
-    //$monitor ("T=%0t rstn=%0b t=%0d q=%0d", $time, rstn, t, q);
+  always #5 clk = ~clk;
+
+  integer f;
+
+  initial begin
+    f = $fopen("output_tff_tb.txt");
+    $fwrite(f, "time,rstn,t,q\n");
+    forever begin
+        @(posedge clk);
+        $fwrite(f, "%g,%b,%b,%b\n", $time,rstn,t,q);
+    end
+
+  end
+
+  initial begin
+    {rstn, clk, t} <= 0;
+    $monitor ("T=%0t rstn=%0b t=%0d q=%0d", $time, rstn, t, q);
     repeat(2) @(posedge clk);
     rstn <= 1;
- 
+
     for (integer i = 0; i < 20; i = i+1) begin
       reg [4:0] dly = $random;
       #(dly) t <= $random;
     end
-  #20 $fclose(f);
+  // #20 $fclose(f);
   #20 $finish;
   end
 endmodule
