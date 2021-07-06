09f191b8e6935f5ee9cae2a483ee52791d22001a
Hammad Ahmad <hammada@umich.edu>
2021-07-06 11:56:39 -0400

    updated tate pairing buggy to remove inc.v and define stuff locally, otherwise we have problems with FL

--- benchmarks/opencores/pairing/tate_pairing_wadden_buggy2.v
+++ benchmarks/opencores/pairing/tate_pairing_wadden_buggy2.v
@@ -18,7 +18,17 @@
     along with Tate Bilinear Pairing Core.  If not, see http://www.gnu.org/licenses/lgpl.txt
 */
  
-`include "inc.v"
+`default_nettype none
+ 
+// drop def & const here :D
+ 
+`define M     97          // M is the degree of the irreducible polynomial
+`define WIDTH (2*`M-1)    // width for a GF(3^M) element
+`define W2    (4*`M-1)    // width for a GF(3^{2*M}) element
+`define W3    (6*`M-1)    // width for a GF(3^{3*M}) element
+`define W6    (12*`M-1)   // width for a GF(3^{6*M}) element
+`define PX    196'h4000000000000000000000000000000000000000001000002 // PX is the irreducible polynomial
+
 `define ZERO {(2*`M){1'b0}}
 `define TWO {(2*`M-2){1'b0}},2'b10
  
