5b0059a19edc1b91d1055f377b49d11fa8a2cab1
Hammad Ahmad <hammada@umich.edu>
2021-11-29 19:03:14 -0500

    removed practice folder

--- practice/first_counter/gen.py
+++
@@ -1,7 +0,0 @@
-tokenfile = "./tokens_temp.txt"
-with open(tokenfile) as f:
-    lines = f.readlines()
-    for line in lines:
-        if line not in ['\n', '\r\n']:
-            token = line.split(' ')[0]
-            print token
